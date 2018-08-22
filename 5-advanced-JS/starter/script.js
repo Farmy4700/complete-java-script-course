7cd642a6d8baacbe5fa12d763f9014a82f5b66da
Farmy4700 <farmy4700@gmail.com>
2018-08-21 19:23:29 -0500

    lesson

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -41,7 +41,7 @@ console.log(rates);
 */
 
 //Functions returning functions
-
+/*
 function interviewQuestion(job) {
     if (job === 'designer') {
         return function(name) {
@@ -82,6 +82,168 @@ var howManyBeersRookie = drunk(3);
 
 howManyBeersPro('Shane');
 howManyBeersRookie('Lori');
+*/
+
+
+
+////////////////////////////////
+//IIFE
+
+/*
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 3);  //data privacy score cannot be accessed outside
+})();
+
+
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 3 - goodLuck);  //data privacy score cannot be accessed outside
+})(5);
+*/
+
+/////////////////////////////
+//Closures
+
+//function how many years left until retirement
+
+function retirement(retiermentAge) {
+    var a = ' years left until retirement.'
+    return function(yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log((retiermentAge - age) + a)
+    }   
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland= retirement(67);
+
+retirementGermany(1979);
+retirementIceland(1987);
+retirementUS(1990);
+
+
+//retirement(66)(2003);
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ' can you explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do ?');
+        }
+    }
+}
+*/
+    function interviewQuestion(job) {
+        return function(name) {
+            if (job === 'designer') {
+                console.log(name + ', can you please explain what UX design is?');
+            } else if (job === 'teacher') {
+                console.log('What subject do you teach, ' + name + '?');
+            } else {
+                console.log('Hello ' + name + ', what do you do?');
+            }
+        }
+    }
+    
+    interviewQuestion('dog walker')('John');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
