11aeef714f4c43a25b7fd599acb7b4ae7c0665bb
Procter <PA8707@dnanico1.aniconet.com>
2018-08-23 13:55:32 -0500

    complete challenge

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -222,17 +222,77 @@ console.log(ages);
 console.log(fullJapan);
 */
 
-var id = "10.10.10.10";
-var privateIP;
-if (/^(10)\.(.*)\.(.*)\.(.*)$/.test(id)){
-  privateIP=(true);
-}else if (/^(172)\.(1[6-9]|2[0-9]|3[0-1])\.(.*)\.(.*)$/.test(id)){
-    privateIP=(true);
-}else if (/^(192)\.(168)\.(.*)\.(.*)$/.test(id)){
-    privateIP=(true);
-}else {
-  privateIP=(false);
+//Coding challenge
+
+(function() { 
+
+function Question (question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
 }
+
+Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+
+    for (var i = 0; i < this.answers.length; i++) {
+        console.log(i + ': ' + this.answers[i]);
+    }
+}
+
+Question.prototype.checkAnswer = function(ans) {
+    if (ans === this.correct) {
+        console.log('Correct Answer!');
+    } else {
+        console.log('wrong answer');
+    }
+}
+
+var q1 = new Question('Is JS the coolest programming language?', 
+    ['Yes', 'No'],
+    0);
+
+var q2 = new Question('What is the name of this courses teacher', 
+    ['John',
+    'Micheal',
+    'Jonas'],
+    1);
+
+var q3 = new Question('What does best describe coding',
+    ['boring',
+    'hard',
+    'fun',
+    'tedious'],
+    2);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer.'));
+
+questions[n].checkAnswer(answer);
+
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
